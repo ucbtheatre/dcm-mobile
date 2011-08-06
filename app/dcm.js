fe6050a313e837b50bf2a180661fa423b0b921b5
John Robert Wilson <johnrobert@gmail.com>
2011-08-06 12:30:29 -0400

    ( Issue #27 ) Encourage users to add to homescreen

--- app/dcm.js
+++ app/dcm.js
@@ -1006,6 +1006,43 @@ DCM.updateFavoriteButtonUI = function(e, data){
     }
 };
 
+//JRW - some code for the add to home screen bubble
+window.addEventListener('load', function() {
+  window.setTimeout(function() {
+    var bubble = new google.bookmarkbubble.Bubble();
+
+    var parameter = 'bmb=1';
+
+    bubble.hasHashParameter = function() {
+      return window.location.hash.indexOf(parameter) != -1;
+    };
+
+    bubble.setHashParameter = function() {
+      if (!this.hasHashParameter()) {
+        window.location.hash += parameter;
+      }
+    };
+
+    bubble.getViewportHeight = function() {
+      return window.innerHeight;
+    };
+
+    bubble.getViewportScrollY = function() {
+      return window.pageYOffset;
+    };
+
+    bubble.registerScrollHandler = function(handler) {
+      window.addEventListener('scroll', handler, false);
+    };
+
+    bubble.deregisterScrollHandler = function(handler) {
+      window.removeEventListener('scroll', handler, false);
+    };
+
+    bubble.showIfAllowed();
+  }, 1000);
+}, false);
+
 
 /*
  * JavaScript Pretty Date

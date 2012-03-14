46291e3a64b9ee99ac88e17b492a727178db3ff3
John Robert Wilson <johnrobert@gmail.com>
2012-03-14 01:35:55 -0400

    Adding skeleton sencha project

---
+++ web-app/app/view/Main.js
@@ -0,0 +1,14 @@
+Ext.define("dcm14.view.Main", {
+    extend: 'Ext.tab.Panel',
+    requires: ['Ext.TitleBar'],
+    
+    config: {
+        tabBarPosition: 'bottom',
+        
+        items: [ 
+          {
+            xtype: 'homepanel'
+          }
+        ]
+    }
+});

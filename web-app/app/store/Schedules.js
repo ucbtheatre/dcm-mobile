44d6ccd394d6bcaced44c85a04878ab7e214463a
Benjamin Ragheb <ben@benzado.com>
2012-04-15 16:58:50 -0400

    Adding a store for schedules

---
+++ web-app/app/store/Schedules.js
@@ -0,0 +1,11 @@
+Ext.define('dcm14.store.Schedules', {
+  extend: 'Ext.data.Store', 
+  config : {
+    model: 'dcm14.model.Schedule',
+    proxy: {
+        type: 'ajax',
+        url : 'dcm13data.json',
+        reader: { type : 'json', rootProperty : 'Schedules', record : 'Schedule' }
+    },
+  }
+});

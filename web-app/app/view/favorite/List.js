d0afc3ff65f8c52d96f941957c6d878b6b9b9998
John Robert Wilson <johnrobert@gmail.com>
2012-06-25 10:06:27 -0400

    Adding default empty text to favorites list

--- web-app/app/view/favorite/List.js
+++ web-app/app/view/favorite/List.js
@@ -1,7 +1,6 @@
 Ext.define('dcm14.view.favorite.List', {
   extend: 'Ext.List',
   xtype: 'favorites',
-  emptyText: 'You have no favorite shows yet.',
   prepareData:function(data, record_index, record) {
     showStore = Ext.getStore('Shows');
     showIndex = showStore.find('id', record.get('show_id'));
@@ -10,7 +9,13 @@ Ext.define('dcm14.view.favorite.List', {
     return return_object;
   },
   config: {
-    itemTpl: ['<div class="favorite"><div class="title">{show_name}</div><div class="x-list-disclosure "></div></div>']
+    itemTpl: ['<div class="favorite"><div class="title">{show_name}</div><div class="x-list-disclosure "></div></div>'],
+    listeners:{
+      afterrender: function(cmp) {
+        cmp.refresh();
+      }
+    },
+    emptyText: 'You have no favorite shows yet.'
   },
   
   initialize: function() {
